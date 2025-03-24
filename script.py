f3125b63172e58c4f0d8045941f9fc5ba61d485a
Francisco Papoula <fc58206@alunos.fc.ul.pt>
2025-03-24 18:24:35 +0000

    Script para popular db

---
+++ script.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+import pandas as pd
+
+# Connect to MongoDB (make sure MongoDB is running)
+client = MongoClient("mongodb://localhost:27017/")
+
+# Create a database and collections for each dataset
+db = client["my_database"]
+movies_collection = db["movies"]
+crew_collection = db["crew"]
+actors_collection = db["actors"]
+themes_collection = db["themes"]
+genres_collection = db["genres"]
+
+
+# Load CSV files using pandas
+movies_df = pd.read_csv("Movies/movies.csv")
+crew_df = pd.read_csv("Movies/crew.csv")
+actors_df = pd.read_csv("Movies/actors.csv")
+themes_df = pd.read_csv("Movies/themes.csv")
+genres_df = pd.read_csv("Movies/genres.csv")
+
+# Insert data into MongoDB
+movies_collection.insert_many(movies_df.to_dict(orient='records'))
+crew_collection.insert_many(crew_df.to_dict(orient='records'))
+actors_collection.insert_many(actors_df.to_dict(orient='records'))
+themes_collection.insert_many(themes_df.to_dict(orient='records'))
+genres_collection.insert_many(genres_df.to_dict(orient='records'))
+
+print("Data loaded into MongoDB!")
