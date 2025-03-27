e6b3452ee4dd605a7c8d86bb412257569ac6b394
Diogo Sargaço <146717547+Dnasar0@users.noreply.github.com>
2025-03-27 17:44:15 +0000

    posters no script

--- script.py
+++ script.py
@@ -11,6 +11,7 @@ crew_collection = db["crew"]
 actors_collection = db["actors"]
 themes_collection = db["themes"]
 genres_collection = db["genres"]
+posters_collection = db["posters"]
 
 # Load CSV files using pandas
 movies_df = pd.read_csv("data/movies.csv")
@@ -18,6 +19,7 @@ crew_df = pd.read_csv("data/crew.csv")
 actors_df = pd.read_csv("data/actors.csv")
 themes_df = pd.read_csv("data/themes.csv")
 genres_df = pd.read_csv("data/genres.csv")
+posters_df = pd.read_csv("data/posters.csv")
 
 # Insert data into MongoDB
 movies_collection.insert_many(movies_df.to_dict(orient='records'))
@@ -25,5 +27,7 @@ crew_collection.insert_many(crew_df.to_dict(orient='records'))
 actors_collection.insert_many(actors_df.to_dict(orient='records'))
 themes_collection.insert_many(themes_df.to_dict(orient='records'))
 genres_collection.insert_many(genres_df.to_dict(orient='records'))
+posters_collection.insert_many(posters_df.to_dict(orient='records'))
+
 
 print("Data loaded into MongoDB!")
